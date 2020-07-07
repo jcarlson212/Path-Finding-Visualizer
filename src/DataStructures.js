789211327c1f9a818dc811d65d1e31a6b72d2b0b
Jason Carlson <jcarlson212@gmail.com>
2020-07-06 23:25:03 -0700

    Made a PriorityQueue class in javascript

--- src/DataStructures.js
+++ src/DataStructures.js
@@ -29,6 +29,111 @@ export class Queue {
     }
 }
 
+export class PriorityQueue {
+    constructor() {
+        this.items = []
+    }
+
+    push(item) {
+        this.items.push(item)
+        this.heapifyUp(this.items.length-1)
+    }
+
+    heapifyUp = (index) => {
+        if(index === 0) return
+        if(this.items[index] < this.items[(index - 1 ) / 2]) {
+            //move it up by swaping
+            const temp = this.items[index]
+            this.items[index] = this.items[(index - 1) / 2]
+            this.items[(index - 1) / 2] = temp
+
+            this.heapifyUp((index - 1) / 2)
+        }
+    }
+
+    heapifyDown = (index) => {
+        if(2*index +1 <= this.items.length -1){
+            //has left child
+            if(2*index+2 <= this.items.length-1){
+                //has right child and left child
+                if(this.items[2*index+1] < this.items[index]){
+                    if(this.items[2*index+2] < this.items[2*index+1]){
+                        //swap
+                        const temp = this.items[2*index+2]
+                        this.items[2*index+2] = this.items[index]
+                        this.items[index] = temp
+
+                        //recurse
+                        this.heapifyDown(2*index+2)
+                    }else{
+                            //swap
+                        const temp = this.items[2*index+1]
+                        this.items[2*index+1] = this.items[index]
+                        this.items[index] = temp
+
+                        //recurse
+                        this.heapifyDown(2*index+1)
+                    }
+                    
+                }else if(this.items[2*index+2] < this.items[index]){
+                    //swap
+                    const temp = this.items[2*index+2]
+                    this.items[2*index+2] = this.items[index]
+                    this.items[index] = temp
+
+                    //recurse
+                    this.heapifyDown(2*index+2)
+                }
+            }else{
+                //only has left child
+                if(this.items[2*index+1] < this.items[index]){
+                    //swap
+                    const temp = this.items[2*index+1]
+                    this.items[2*index+1] = this.items[index]
+                    this.items[index] = temp
+
+                    //recurse
+                    this.heapifyDown(2*index+1)
+                }
+            }
+        }else{
+            //has no children
+            return
+        }
+    }
+
+    pop() {
+        if(this.isEmpty()){
+            return null
+        }
+        
+
+        //maintain heap property
+        const temp = this.items[0]
+        this.items[0] = this.items[this.items.length-1]
+        this.items[this.items.length-1] = temp
+        
+        const removedItem = this.items[this.items.length-1]
+        this.items.length = this.items.length-1
+
+
+        this.heapifyDown(0)
+
+        return removedItem
+    }
+
+    front() {
+        if(this.isEmpty()){
+            return null
+        }
+        return this.items[0]
+    }
+
+    isEmpty() {
+        return this.items.length === 0
+    }
+}
+
 export class Stack {
     constructor() {
         this.items = []
